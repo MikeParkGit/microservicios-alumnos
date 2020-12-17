fac94ece0c23cbe10e3c894d19c0e19c082b2252
MikeParkTest <miguelcfb@gmail.com>
2020-12-17 14:00:15 -0600

    Entity Alumno
    Creacion del entity alumno

---
+++ microservicios-alumnos/src/main/java/mcm/personal/microservicios/app/alumnos/models/entity/Alumno.java
@@ -0,0 +1,56 @@
+package mcm.personal.microservicios.app.alumnos.models.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Alumno {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column
+	private String nombre;
+	
+	@Column
+	private String apellido;
+	
+	@Column(nullable = false)
+	private LocalDateTime createAt;
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public LocalDateTime getCreateAt() {
+		return createAt;
+	}
+	public void setCreateAt(LocalDateTime createAt) {
+		this.createAt = createAt;
+	}
+
+	
+}
